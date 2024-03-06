784e1b8f6e6e36fd8e2b123679d03496014e5e61
350Tracyprogram777 <150111144+350Tracyprogram777@users.noreply.github.com>
2024-03-06 16:10:28 +0800

    Update insert_sort.cpp

--- insert_sort.cpp
+++ insert_sort.cpp
@@ -1,1 +1,28 @@
-/插入排序
+//插入排序
+#include<vector>
+#include<iostream>
+using namespace std;
+void Insert_Sort(vector<int>& n)
+{
+	int i,j,k;
+	for ( i = 1; i < n.size(); i++)
+	{
+		k = n[i];
+		j = i - 1; 
+		while (j>=0 && n[j]>k)
+		{
+			n[j + 1] = n[j];
+			j--;
+		}
+		n[j + 1] = k;
+	}
+}
+int main() {
+	vector<int> v = { 5,3,4,1,2 };
+	Insert_Sort(v);
+	for (auto i = 0; i <= v.size(); i++)
+	{
+		cout << v[i] << " ,";
+	}
+	return 0;
+}
